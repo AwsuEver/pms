5d9d2c475ae259b96b48e8457437a14f7bba1700
AwsuEver <945565237@qq.com>
2022-03-23 20:47:38 +0800

    配置项目

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from 'antd'
 
 function App() {
   return (
     <div className="App">
       <div>hello world</div>
+      <Button>点击</Button>
     </div>
   );
 }
