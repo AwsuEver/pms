dcc66172a7e603535a30e6cb856e2e614d07285f
AwsuEver <945565237@qq.com>
2022-03-29 19:26:22 +0800

    修改配置文件

--- config-overrides.js
+++ config-overrides.js
@@ -5,17 +5,20 @@ const path = require('path')
 module.exports = override(   
   // 配置antd使用less修改样式
   fixBabelImports('import', {
-    libraryName: 'antd',
+    libraryName: 'antd',    
     libraryDirectory: 'es',
+    // style: 'css'
     style: true,
   }),
   // 修改antd默认样式
-  addLessLoader({
-    lessOptions: {
-      javascriptEnabled:true ,
-      ModifyVars:{  '@primary-color': '#1DA57A'  } 
-    },
-  }),
+  // addLessLoader({
+  //   lessOptions: {
+  //     javascriptEnabled:true ,
+  //     ModifyVars:{
+  //       '@primary-color': '#1DA57A'
+  //     } 
+  //   },
+  // }),    
   // 配置路径别名
   addWebpackAlias({
     ['@']: path.resolve(__dirname, './src')
